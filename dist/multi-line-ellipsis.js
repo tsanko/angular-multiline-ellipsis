8cfe9e0d0be3df4dea9c18313b4c066d2f310acd
Tsanko Tsolov <ttsolov@rexchange.com>
2015-07-10 10:20:50 -0700

    Added DIST

---
+++ dist/multi-line-ellipsis.js
@@ -0,0 +1,65 @@
+/*!
+ *  Many thanks to Mobify http://www.mobify.com/blog/multiline-ellipsis-in-pure-css/
+ *  for this brilliant CSS trick
+ */
+/**
+ * @ngdoc controller
+ * @name rexWebApp.controllers:EllipsisController
+ * @description
+ * # EllipsisController
+ */
+angular.module('TT.multilineEllipsis', []).controller('EllipsisController', EllipsisController);
+/* @ngInject */
+function EllipsisController() {
+  'use strict';
+  var ellipsis = this;
+  ellipsis.viewMore = false;
+  ellipsis.viewHeight = 75;
+  ellipsis.viewLineHeight = 25;
+  ellipsis.viewMoreDescr = viewMoreDescr;
+  init();
+  ////////////////
+  function init() {
+    ellipsis.viewHeight = ellipsis.multiLineEllipsisLines * ellipsis.viewLineHeight;
+  }
+  function viewMoreDescr() {
+    ellipsis.viewMore = true;
+  }
+}/**
+ * @ngdoc directive
+ * @name rexWebApp.directive:multiLineEllipsis
+ * @restrict A
+ * @scope
+ *
+ * @description
+ * Directive to apply ellipsis to a multi line text.
+ *
+ * @usage
+ * <div multi-line-ellipsis multi-line-ellipsis-lines="3" >
+ *     Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.
+ *     -- A. Einstein
+ * </div>
+ *
+ * @example
+ */
+angular.module('TT.multilineEllipsis').directive('multiLineEllipsis', multiLineEllipsis);
+/* @ngAnnotate */
+function multiLineEllipsis() {
+  'use strict';
+  return {
+    templateUrl: 'template/multi-line-ellipsis.tpl.html',
+    transclude: true,
+    replace: true,
+    scope: { multiLineEllipsisLines: '@' },
+    controller: 'EllipsisController',
+    controllerAs: 'ellipsis',
+    bindToController: true
+  };
+}
+angular.module('TT.multilineEllipsis', []).run([
+  '$templateCache',
+  function ($templateCache) {
+    'use strict';
+    $templateCache.put('template/multi-line-ellipsis.html', '< div > Tsanko < / div > ');
+  }
+]);
