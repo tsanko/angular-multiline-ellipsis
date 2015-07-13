2fc4dd6e507848f10e333ca7a54979eca23e2346
Tsanko Tsolov <ttsolov@rexchange.com>
2015-07-13 09:55:52 -0700

    Remove CSS font specifics

---
+++ src/multi-line-ellipsis.tpl.js
@@ -0,0 +1,24 @@
+angular
+	.module('TT.multiLineEllipsis')
+	.run(['$templateCache', function ($templateCache) {
+
+		'use strict';
+
+		$templateCache.put(
+			'template/multi-line-ellipsis.html',
+			'<div class="ellipsis"' +
+				' ng-style="(ellipsis.viewMore) && { \'height\' : \'auto\' }"' +
+				' style="height : {{ ::ellipsis.viewHeight }}px; line-height : {{ ::ellipsis.viewLineHeight }}px;" >' +
+
+				'<div class="before" style="height : {{ ::ellipsis.viewHeight }}px;" ></div >' +
+				'<div class="body" >' +
+					'<div ng-transclude ></div >' +
+				'</div >' +
+				'<div class="after"' +
+					' style="top : -{{ ::ellipsis.viewLineHeight }}px;"' +
+					' ng-click="ellipsis.viewMoreDescr()"' +
+					' ng-if="!ellipsis.viewMore" > ... View More' +
+				'</div >' +
+			'</div >'
+		);
+	}]);
